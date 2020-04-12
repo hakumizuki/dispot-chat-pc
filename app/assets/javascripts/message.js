f8b5fc9d8f8955be2c27e26173785ce9fc70e6ab
Taichi Masuyama <montanha.masu536@gmail.com>
2020-04-13 00:09:18 +0900

    復興完了
    禿げるかと思った、、、。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,7 +40,7 @@ $(function(){
     };
   }
 
-  $('#new_message').on('submit', function(e){
+  $('#new_message').on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
